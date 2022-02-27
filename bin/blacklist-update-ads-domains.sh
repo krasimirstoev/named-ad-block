c3095a81ca7c5af1f0e66d15290c90d995f2d43e
Krasimir Stoev <alabalism@gmail.com>
2022-02-27 23:22:39 +0200

    auto commit: нд фев 27 23:22:39 EET 2022

--- bin/blacklist-update-ads-domains.sh
+++ bin/blacklist-update-ads-domains.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
 
-wget --no-check-certificate -O /var/named/blacklists/ads 'http://pgl.yoyo.org/adservers/serverlist.php?hostformat=bindconfig&showintro=0&mimetype=plaintext'
+rm -f /var/named/blacklists/ads
+
+wget --quiet --no-check-certificate -O /var/named/blacklists/ads 'http://pgl.yoyo.org/adservers/serverlist.php?hostformat=bindconfig&showintro=0&mimetype=plaintext'
